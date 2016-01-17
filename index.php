19523017e5cabb147ce36b0adda14b83c23d8eec
Kevin Scozzari <scozzari013@gmail.com>
2016-01-17 15:34:12 +0100

    Small update

---
+++ index.php
@@ -0,0 +1,95 @@
+<! DOCTYPE HTML>
+<html lang="nl">
+<head>
+<title>SeoSolution - Get higher in Google</title>
+
+<body style="background-image: url('/css/pics/bg.jpg'); background-position:center top; background-attachment:fixed; background-size: 100%;">
+
+<!-- Metatags -->
+<meta name="description" content="SeoSolution">
+<meta name="keywords" content="seosolution, kevin scozzari, get higher in Google">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="author" content="Kevin Scozzari">
+
+<!-- CSS -->
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" href="http://seosolution.guthub.io/css/pics/favicon.ico" />
+<!-- JavaScript -->
+<script src="/js/jquery-1.11.2.min.js"></script>
+<script src="/js/scroll.js"></script>
+
+<!-- Don't steal my stuff bruh -->
+<link type="text/plain" rel="author" href="http://seosolution.gihub.io/humans.txt">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+<body>
+<!--
+===================================================================
+ Scroll to top button
+===================================================================
+-->
+<div class="scroll-top-wrapper no-print">
+	<span class="scroll-top-inner">
+		<i class="fa fa-2x fa-arrow-up"></i>
+	</span>
+</div>
+
+<div id="header">
+<br>
+<br>
+<center>
+	<p><span style="font-size: 25">SeoSolution</span></p>
+	<p><span style="font-style: Italic">Get higher in <i class="fa fa-google"></i>oogle.</span>
+</center>
+
+
+
+<div class="col-sm-12">
+<p>We're re-coding our new website</p>
+<br><br>
+
+<br><br><br>
+<div class="row">
+<div class="col-sm-6">
+<p>
+<b><big><u>---</u></big></b><br>
+...
+</p>
+</div>
+<div class="col-sm-6">
+<p>
+<b><big><u>---</u></big></b><br>
+...
+</div>
+</p>
+<br><br>
+<div class="row">
+<div class="col-sm-4">
+<p>
+<b><big><u>---</u></big></b><br><br>
+...
+</div>
+<br><br><br><br><br><br><br><br>
+<div class="tleft">
+<b><big><u>---</u></big></b><br>
+...
+</div></div>
+<br><br>
+
+</div>
+<br><br><br><br><br><br><br><br><br>
+<center>
+
+<p>&copy; SeoSolution 2016</p>
+</center>
+</body>
+</html>
