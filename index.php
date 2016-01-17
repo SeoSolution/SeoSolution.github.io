fd5660820ad335bc5ce2debbd658769ffe12a61c
Kevin Scozzari <scozzari013@gmail.com>
2016-01-17 15:28:56 +0100

    Big update

---
+++ index.php
@@ -0,0 +1,116 @@
+<! DOCTYPE HTML>
+<html lang="nl">
+<head>
+<title>SeoSolution - Get higher in Google</title>
+
+<body style="background-image: url('/css/pics/bg.jpg'); background-position:center top; background-attachment:fixed; background-size: 100%;">
+
+<!-- Metatags -->
+<meta name="description" content="SeoSolution">
+<meta name="keywords" content="seosolution, kevin scozzari, get higher in Google">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="author" content="Kevin Scozzari">
+
+<!-- CSS -->
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" href="http://seosolution.guthub.io/css/pics/favicon.ico" />
+<!-- JavaScript -->
+<script src="/js/jquery-1.11.2.min.js"></script>
+<script src="/js/scroll.js"></script>
+
+<!-- Don't steal my stuff bruh -->
+<link type="text/plain" rel="author" href="http://seosolution.gihub.io/humans.txt">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+<body>
+<!--
+===================================================================
+ Scroll to top button
+===================================================================
+-->
+<div class="scroll-top-wrapper no-print">
+	<span class="scroll-top-inner">
+		<i class="fa fa-2x fa-arrow-up"></i>
+	</span>
+</div>
+
+<div id="header">
+<br>
+<br>
+<center>
+	<h2>SeoSolution</h2>
+	<p><span style="font-style: 'Italic'">Get higher in <i class="fa fa-google"></i>oogle.</span>
+</center>
+
+
+
+<div class="col-sm-12">
+<p>We're re-coding our new website</p>
+<br><br>
+
+<br><br><br>
+<div class="row">
+<div class="col-sm-6">
+<p>
+<b><big><u>Uitzendingen</u></big></b><br>
+<?php
+$a=array("red","green","blue","yellow","brown");
+$random_keys=array_rand($a,3);
+echo $a[$random_keys[0]]."<br>";
+echo $a[$random_keys[1]]."<br>";
+echo $a[$random_keys[2]];
+?>
+</p>
+</div>
+<div class="col-sm-6">
+<p>
+<b><big><u>Nieuws</u></big></b><br>
+5 februari om 5 uur - De top 15 beste carnaval hits van 2016.<br>
+14 februari - De beste Valentijn gedichten & 24 non-stop Liefdes muziek.<br>
+24 april t/m 1 mei - Music for Life KWF Actie samen sterk voor KWF.
+</div>
+</p>
+<br><br>
+<div class="row">
+<div class="col-sm-4">
+<p>
+<b><big><u>Verzoeknummer</u></big></b><br><br>
+<a href="https://docs.google.com/forms/d/1V2JwmKdRDis4pzotC0wczqk4EVyPc48e35Wq-BEp7bA/viewform" target="_blank">Aanvragen</a></p>
+</div>
+<br><br><br><br><br><br><br><br>
+<div class="tleft">
+<b><big><u>Nieuwsbrief</u></big></b><br>
+De nieuwsbrief wordt binnenkort pas geleverd.<br>
+Een keer per maand versturen wij een nieuwsbrief.<br>
+De nieuwsbrief bevat nieuws en het vrak v/d maand.<br> 
+Schrijf je in voor onze nieuwsbrief TheBestFriends!<br><br>
+<a href="https://docs.google.com/forms/d/1i8tL07rciLa3bQngbEJl1HbkIXzjNuP15E8FC9I-ir8/viewform" target="_blank">Inschrijven</a>
+</div></div>
+<br><br>
+
+</div>
+
+<br><br><br>
+<p>SeoSolution</a></p>
+<p><span style="font-size: 12px">Get higher in <i class="fa fa-google"></i>oogle.</span></p>
+<p><span style="font-size: 10px"><a href="http://seosolution.github.io/">Our website.</a></span></p>
+<br><br><br>
+<center>
+	<a href="http://fb.com/Thebestdigitaal-1656645414612054" target="_blank"><i class="fa fa-facebook fa-2x"></i></a>
+	<i class="fa fa-youtube fa-2x"></i>
+	<a href="http://twitter.com/thebestdigitaal" target="_blank"><i class="fa fa-twitter fa-2x"></i></a>
+	<br><br><br><br>
+
+<p>&copy; TheBestDigitaal 2016</p>
+</center>
+</body>
+</html>
